1675f44e7685eb6b0ce3574ca4bb9d0f4de1f883
sasmb <ogbsamueljm@gmail.com>
2025-04-13 10:18:33 +0300

    fix: configure Redis SSL for Heroku with proper certificate verification

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,12 +3,12 @@ module RedisConfig
     {
       url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'),
       ssl_params: {
-        ca_file: ENV['REDIS_CA_FILE'],
-        verify_mode: ENV['REDIS_OPENSSL_VERIFY'].to_i == 0 ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER,
+        verify_mode: OpenSSL::SSL::VERIFY_PEER,
         ssl_version: :TLSv1_2
-      },
+      }.compact,
       network_timeout: 5,
-      pool_timeout: 5
-    }
+      pool_timeout: 5,
+      ssl: true  # Heroku Redis requires SSL
+    }.compact
   end
 end 
